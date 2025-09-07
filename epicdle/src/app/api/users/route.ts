ed267f933bbd5ec0da3f47955b17bd15d7460154
Dylan Vu <dylanvu9@gmail.com>
2025-09-07 01:27:08 -0700

    users is not part of MVP

--- epicdle/src/app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET(request: Request) {
-  const testMessage = {
-    message: `Hello World! It is ${Date.now()}.`,
-  };
-  return new Response(JSON.stringify(testMessage), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
