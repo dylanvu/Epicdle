a88655e17c59729f304cc64ffc66cfc5927dc28c
Dylan Vu <dylanvu9@gmail.com>
2025-10-04 21:14:19 -0700

    add tutorial translation

--- epicdle/src/components/TutorialModal/TutorialModal.tsx
+++ epicdle/src/components/TutorialModal/TutorialModal.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { Button, Modal, Text } from "@mantine/core";
 import { UseDisclosureHandlers } from "@mantine/hooks";
 import styles from "./TutorialModal.module.css";
 import { MAX_GUESSES } from "@/constants";
 import { PRIMARY_COLOR } from "@/theme";
 import { useButtonSound } from "@/audio/playButtonSound";
+import { useState } from "react";
 
 export default function TutorialModal({
   openState,
@@ -13,6 +15,7 @@ export default function TutorialModal({
   modalHandler: UseDisclosureHandlers;
 }) {
   const playButtonSound = useButtonSound();
+  const [showRealTutorial, setShowRealTutorial] = useState(false);
   return (
     <Modal
       opened={openState}
@@ -23,12 +26,39 @@ export default function TutorialModal({
       title="How to Play"
       className={styles.game}
     >
-      <Text>You have a challenge, a test of skill</Text>
-      <Text>A song to name that will contest your will</Text>
-      <Text>
-        There are {MAX_GUESSES.toString()} tries to reach win's thrill
-      </Text>
-      <Text>Each guess brings you close as the music spills</Text>
+      {showRealTutorial ? (
+        <div>
+          <Text>
+            Try to guess the song in {MAX_GUESSES.toString()} or fewer tries.
+          </Text>
+          <Text>Each attempt will reveal more of the song.</Text>
+          <Text>Good luck!</Text>
+        </div>
+      ) : (
+        <div>
+          <Text>You have a challenge, a test of skill</Text>
+          <Text>A song to name that will contest your will</Text>
+          <Text>
+            There are {MAX_GUESSES.toString()} tries to reach win's thrill
+          </Text>
+          <Text>Each guess brings you close as the music spills</Text>
+        </div>
+      )}
+
+      {!showRealTutorial ? (
+        <Button
+          onClick={() => {
+            playButtonSound();
+            setShowRealTutorial(true);
+          }}
+          mt="md"
+          w="100%"
+          variant="light"
+          color={PRIMARY_COLOR}
+        >
+          ...What?
+        </Button>
+      ) : null}
       <Button
         onClick={() => {
           playButtonSound();
