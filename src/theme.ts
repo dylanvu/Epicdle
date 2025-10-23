1fb9f52a5187db7c90d7cc9117616e8f19b192fa
Dylan Vu <dylanvu9@gmail.com>
2025-10-22 20:01:30 -0700

    add focus color

--- src/theme.ts
+++ src/theme.ts
@@ -2,13 +2,15 @@
 
 import { createTheme } from "@mantine/core";
 
-export const theme = createTheme({
-  /* Put your mantine theme override here */
-});
-
 // export const PRIMARY_COLOR = "#4A6572";
 export const PRIMARY_COLOR = "#3B7A8C";
 export const WRONG_COLOR = "#A55A5A";
 export const WIN_COLOR = "#D4AF7F";
 
 export const CONFETTI_COLORS = ["#D4AF7F", "#E08C79", "#7FB3B3"];
+
+export const theme = createTheme({
+  /* Put your mantine theme override here */
+  fontFamily: "'Cinzel', sans-serif",
+  focusClassName: "focus",
+});
