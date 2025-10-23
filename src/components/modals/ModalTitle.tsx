d18e059561bb2be9484d83ac71cb1a34fb1027f9
Dylan Vu <dylanvu9@gmail.com>
2025-10-22 21:23:20 -0700

    apply cinzel font to modal titles

--- src/components/modals/ModalTitle.tsx
+++ src/components/modals/ModalTitle.tsx
@@ -1,9 +1,14 @@
 import { Text } from "@mantine/core";
+import { FONT_CLASS_NAME } from "@/components/Text/Epicdle/EpicdleCommon";
 
 export default function ModalTitle({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Text size={"xl"}>{children}</Text>;
+  return (
+    <Text size={"xl"} className={FONT_CLASS_NAME}>
+      {children}
+    </Text>
+  );
 }
