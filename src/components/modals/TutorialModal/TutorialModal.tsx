bf729bacbbd404d7e25a744001106d85b3fca445
Dylan Vu <dylanvu9@gmail.com>
2026-01-07 23:47:28 -0800

    tweak tutorial for instrumental mode

--- src/components/modals/TutorialModal/TutorialModal.tsx
+++ src/components/modals/TutorialModal/TutorialModal.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { Button, List, Modal, Stack, Text, ThemeIcon } from "@mantine/core";
+import { Anchor, Button, List, Modal, Stack, Text, ThemeIcon } from "@mantine/core";
 import { UseDisclosureHandlers } from "@mantine/hooks";
 import styles from "./TutorialModal.module.css";
-import { MAX_GUESSES } from "@/constants";
+import { MAX_GUESSES, SUPPORT_EMAIL } from "@/constants";
 import { PRIMARY_COLOR } from "@/config/theme";
 import { useButtonSound } from "@/hooks/audio/useButtonSound";
 import ModalTitle from "../ModalTitle";
@@ -52,8 +52,8 @@ export default function TutorialModal({
             <>
               <Text>Give me rhythms and a drumbeat</Text>
               <Text>Give me tempos and a motif</Text>
-              <Text>Without the lyrics songs are scary</Text>
-              <Text>But I wanna be legendary</Text>
+              <Text>Without the lyrics, songs are scary</Text>
+              <Text>But I wanna be legendary!</Text>
             </>
           ) : (
             <>
